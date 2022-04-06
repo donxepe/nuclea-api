0ceb3b2d310f46556b367ad587c26b0bbbd246b1
Luis Zepeda <thexepe@gmail.com>
2022-04-06 16:14:46 -0500

    create db connection

---
+++ src/db.js
@@ -0,0 +1,39 @@
+require('dotenv').config;
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios')
+const { Sequelize } = require('sequelize')
+
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/nuclea`, {
+    logging: false
+});
+
+const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+// adds all models in src/models to the modelDefiners array
+fs.readdirSync(path.join(__dirname, '/models'))
+  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+  });
+
+modelDefiners.forEach(model => model(sequelize));
+
+// capitalize models' names
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => 
+    [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+sequelize.models = Object.fromEntries(capsEntries);
+
+// Declare relationships
+
+
+
+module.exports = {
+    ...sequelize.models,
+    conn: sequelize
+};
